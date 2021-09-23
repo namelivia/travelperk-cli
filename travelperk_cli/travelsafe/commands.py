9a638f89445cfe0172db2e62499803684548d830
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-09-23 20:35:04 +0200

    Implement travelsafe commands

--- travelperk_cli/travelsafe/commands.py
+++ travelperk_cli/travelsafe/commands.py
@@ -1,4 +1,10 @@
 import click
+import json
+from travelperk_cli.travelperk.travelperk import get_backend
+from travelperk_http_python.exceptions.travelperk_http_exception import (
+    TravelPerkHttpException,
+)
+from pydantic.json import pydantic_encoder
 
 
 @click.group()
@@ -7,24 +13,79 @@ def travelsafe():
 
 
 @click.command()
-def restrictions():
-    click.echo("Listing travel restrictions")
+@click.option("--origin", help="The location code for the origin.", required=True)
+@click.option(
+    "--origin-type",
+    help="The type for the location code for the origin.",
+    required=True,
+)
+@click.option(
+    "--destination", help="The location code for the destination.", required=True
+)
+@click.option(
+    "--destination-type",
+    help="The type for the location code for the destination.",
+    required=True,
+)
+@click.option(
+    "--date",
+    help="The date for the travel.",
+    type=click.DateTime(formats=["%Y-%m-%d"]),
+    required=True,
+)
+def restrictions(origin, destination, origin_type, destination_type, date):
+    try:
+        click.echo(
+            json.dumps(
+                get_backend()
+                .travelsafe()
+                .travelsafe()
+                .travel_restrictions(
+                    origin, destination, origin_type, destination_type, date
+                ),
+                default=pydantic_encoder,
+            )
+        )
+    except TravelPerkHttpException as e:
+        click.echo(click.style(str(e), fg="red"))
 
 
 travelsafe.add_command(restrictions)
 
 
 @click.command()
-def local_summary():
-    click.echo("Listing local summary")
+@click.option("--location", help="The location code.", required=True)
+@click.option("--location-type", help="The type for the location code.", required=True)
+def local_summary(location, location_type):
+    try:
+        click.echo(
+            json.dumps(
+                get_backend()
+                .travelsafe()
+                .travelsafe()
+                .local_summary(location, location_type),
+                default=pydantic_encoder,
+            )
+        )
+    except TravelPerkHttpException as e:
+        click.echo(click.style(str(e), fg="red"))
 
 
 travelsafe.add_command(local_summary)
 
 
 @click.command()
-def airline_measures():
-    click.echo("Listing airline measures")
+@click.option("--code", help="The airline code.", required=True)
+def airline_measures(code):
+    try:
+        click.echo(
+            json.dumps(
+                get_backend().travelsafe().travelsafe().airline_safety_measures(code),
+                default=pydantic_encoder,
+            )
+        )
+    except TravelPerkHttpException as e:
+        click.echo(click.style(str(e), fg="red"))
 
 
 travelsafe.add_command(airline_measures)
