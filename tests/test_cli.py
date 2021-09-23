65d3de8d897faf69ecea9e2faa8b4b437768ee97
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2021-09-23 22:56:20 +0200

    Add even more assertions

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 from click.testing import CliRunner
 from cli import cli
 from mock import Mock, patch
@@ -45,6 +46,7 @@ class TestCli:
         result = self.runner.invoke(cli, ["cost-centers", "all"])
         assert result.exit_code == 0
         assert "All cost centers list" in result.output
+        self.travelperk.cost_centers.return_value.cost_centers.return_value.all.assert_called_once_with()
 
     @patch("travelperk_cli.cost_centers.commands.get_backend")
     def test_getting_details_of_a_cost_center(self, get_backend_mock):
@@ -58,7 +60,6 @@ class TestCli:
         )
         assert result.exit_code == 0
         assert "Cost center details" in result.output
-        # TODO
         self.travelperk.cost_centers.return_value.cost_centers.return_value.get.assert_called_once_with(
             cost_center_id
         )
@@ -69,14 +70,15 @@ class TestCli:
         self.travelperk.cost_centers.return_value.cost_centers.return_value.create.return_value = (
             "New cost center"
         )
-        cost_center_name = "new_cost_center"
+        cost_center_name = "New cost center name"
         result = self.runner.invoke(
             cli, ["cost-centers", "create", "--name", cost_center_name]
         )
         assert result.exit_code == 0
         assert "New cost center" in result.output
-        # TODO
-        # asserting the function was called with the id
+        self.travelperk.cost_centers.return_value.cost_centers.return_value.create.assert_called_once_with(
+            cost_center_name
+        )
 
     # Expenses
     def test_listing_expenses_operations(self):
@@ -100,6 +102,7 @@ class TestCli:
         result = self.runner.invoke(cli, ["expenses", "invoice-profiles", "all"])
         assert result.exit_code == 0
         assert "All invoice profiles list" in result.output
+        self.travelperk.expenses.return_value.invoice_profiles.return_value.query.return_value.get.assert_called_once_with()
 
     # Invoices
     def test_listing_invoices_operations(self):
@@ -118,6 +121,7 @@ class TestCli:
         result = self.runner.invoke(cli, ["expenses", "invoices", "all"])
         assert result.exit_code == 0
         assert "All invoices list" in result.output
+        self.travelperk.expenses.return_value.invoices.return_value.query.return_value.get.assert_called_once_with()
 
     @patch("travelperk_cli.expenses.invoices.commands.get_backend")
     def test_getting_details_of_an_invoice(self, get_backend_mock):
@@ -131,8 +135,9 @@ class TestCli:
         )
         assert result.exit_code == 0
         assert "Invoice details" in result.output
-        # TODO
-        # asserting the function was called with the id
+        self.travelperk.expenses.return_value.invoices.return_value.get.assert_called_once_with(
+            invoice_id
+        )
 
     @patch("travelperk_cli.expenses.invoices.commands.get_backend")
     def test_listing_all_invoice_lines(self, get_backend_mock):
@@ -143,6 +148,7 @@ class TestCli:
         result = self.runner.invoke(cli, ["expenses", "invoices", "lines"])
         assert result.exit_code == 0
         assert "All invoice lines list" in result.output
+        self.travelperk.expenses.return_value.invoices.return_value.lines_query.return_value.get.assert_called_once_with()
 
     # SCIM
     def test_listing_scim_operations(self):
@@ -168,6 +174,7 @@ class TestCli:
         result = self.runner.invoke(cli, ["scim", "users", "all"])
         assert result.exit_code == 0
         assert "All users list" in result.output
+        self.travelperk.scim.return_value.users.return_value.query.return_value.get.assert_called_once_with()
 
     @patch("travelperk_cli.scim.users.commands.get_backend")
     def test_getting_details_of_a_user(self, get_backend_mock):
@@ -179,8 +186,9 @@ class TestCli:
         result = self.runner.invoke(cli, ["scim", "users", "get", "--id", user_id])
         assert result.exit_code == 0
         assert "Details of a user" in result.output
-        # TODO
-        # asserting the function was called with the id
+        self.travelperk.scim.return_value.users.return_value.get.assert_called_once_with(
+            user_id
+        )
 
     @patch("travelperk_cli.scim.users.commands.get_backend")
     def test_deleting_a_user(self, get_backend_mock):
@@ -192,8 +200,9 @@ class TestCli:
         result = self.runner.invoke(cli, ["scim", "users", "delete", "--id", user_id])
         assert result.exit_code == 0
         assert "Delete user response" in result.output
-        # TODO
-        # asserting the function was called with the id
+        self.travelperk.scim.return_value.users.return_value.delete.assert_called_once_with(
+            user_id
+        )
 
     # Discovery
     def test_listing_discovery_operations(self):
@@ -216,6 +225,7 @@ class TestCli:
         )
         assert result.exit_code == 0
         assert "Service provider configuration" in result.output
+        self.travelperk.scim.return_value.discovery.return_value.service_provider_config.assert_called_once_with()
 
     @patch("travelperk_cli.scim.discovery.commands.get_backend")
     def test_getting_resource_types(self, get_backend_mock):
@@ -226,6 +236,7 @@ class TestCli:
         result = self.runner.invoke(cli, ["scim", "discovery", "resource-types"])
         assert result.exit_code == 0
         assert "Resource types" in result.output
+        self.travelperk.scim.return_value.discovery.return_value.resource_types.assert_called_once_with()
 
     @patch("travelperk_cli.scim.discovery.commands.get_backend")
     def test_getting_schemas(self, get_backend_mock):
@@ -236,6 +247,7 @@ class TestCli:
         result = self.runner.invoke(cli, ["scim", "discovery", "schemas"])
         assert result.exit_code == 0
         assert "List of schemas" in result.output
+        self.travelperk.scim.return_value.discovery.return_value.schemas.assert_called_once_with()
 
     @patch("travelperk_cli.scim.discovery.commands.get_backend")
     def test_getting_user_schema(self, get_backend_mock):
@@ -246,6 +258,7 @@ class TestCli:
         result = self.runner.invoke(cli, ["scim", "discovery", "user-schema"])
         assert result.exit_code == 0
         assert "User schema" in result.output
+        self.travelperk.scim.return_value.discovery.return_value.user_schema.assert_called_once_with()
 
     @patch("travelperk_cli.scim.discovery.commands.get_backend")
     def test_getting_enterprise_schema(self, get_backend_mock):
@@ -256,6 +269,7 @@ class TestCli:
         result = self.runner.invoke(cli, ["scim", "discovery", "enterprise-schema"])
         assert result.exit_code == 0
         assert "Enterprise schema" in result.output
+        self.travelperk.scim.return_value.discovery.return_value.enterprise_user_schema.assert_called_once_with()
 
     # TravelSafe
     def test_listing_travelsafe_operations(self):
@@ -271,27 +285,37 @@ class TestCli:
         self.travelperk.travelsafe.return_value.travelsafe.return_value.travel_restrictions.return_value = (
             "Travel restrictions"
         )
+        origin = "FR"
+        origin_type = "country_code"
+        destination = "ES"
+        destination_type = "country_code"
+        date = "2020-05-02"
         result = self.runner.invoke(
             cli,
             [
                 "travelsafe",
                 "restrictions",
                 "--origin",
-                "FR",
+                origin,
                 "--origin-type",
-                "country_code",
+                origin_type,
                 "--destination",
-                "ES",
+                destination,
                 "--destination-type",
-                "country_code",
+                destination_type,
                 "--date",
-                "2020-05-02",
+                date,
             ],
         )
         assert result.exit_code == 0
         assert "Travel restrictions" in result.output
-        # TODO
-        # asserting the function was called with the id
+        self.travelperk.travelsafe.return_value.travelsafe.return_value.travel_restrictions.assert_called_once_with(
+            origin,
+            destination,
+            origin_type,
+            destination_type,
+            datetime(2020, 5, 2, 0, 0),
+        )
 
     @patch("travelperk_cli.travelsafe.commands.get_backend")
     def test_getting_local_summary(self, get_backend_mock):
@@ -299,21 +323,24 @@ class TestCli:
         self.travelperk.travelsafe.return_value.travelsafe.return_value.local_summary.return_value = (
             "Local Summary"
         )
+        location = "FR"
+        location_type = "country_code"
         result = self.runner.invoke(
             cli,
             [
                 "travelsafe",
                 "local-summary",
                 "--location",
-                "FR",
+                location,
                 "--location-type",
-                "country_code",
+                location_type,
             ],
         )
         assert result.exit_code == 0
         assert "Local Summary" in result.output
-        # TODO
-        # asserting the function was called with the id
+        self.travelperk.travelsafe.return_value.travelsafe.return_value.local_summary.assert_called_once_with(
+            location, location_type
+        )
 
     @patch("travelperk_cli.travelsafe.commands.get_backend")
     def test_getting_airline_measures(self, get_backend_mock):
@@ -321,19 +348,21 @@ class TestCli:
         self.travelperk.travelsafe.return_value.travelsafe.return_value.airline_safety_measures.return_value = (
             "Airline Measures"
         )
+        airline_code = "LH"
         result = self.runner.invoke(
             cli,
             [
                 "travelsafe",
                 "airline-measures",
                 "--code",
-                "LH",
+                airline_code,
             ],
         )
         assert result.exit_code == 0
         assert "Airline Measures" in result.output
-        # TODO
-        # asserting the function was called with the id
+        self.travelperk.travelsafe.return_value.travelsafe.return_value.airline_safety_measures.assert_called_once_with(
+            airline_code
+        )
 
     # Trips
     def test_listing_trips_operations(self):
@@ -351,6 +380,7 @@ class TestCli:
         result = self.runner.invoke(cli, ["trips", "all"])
         assert result.exit_code == 0
         assert "All trips list" in result.output
+        self.travelperk.trips.return_value.trips.return_value.query.return_value.get.assert_called_once_with()
 
     @patch("travelperk_cli.trips.commands.get_backend")
     def test_listing_all_bookings(self, get_backend_mock):
@@ -361,6 +391,7 @@ class TestCli:
         result = self.runner.invoke(cli, ["trips", "bookings"])
         assert result.exit_code == 0
         assert "All bookings list" in result.output
+        self.travelperk.trips.return_value.bookings.return_value.query.return_value.get.assert_called_once_with()
 
     # Users
     def test_listing_users_operations(self):
@@ -377,6 +408,7 @@ class TestCli:
         result = self.runner.invoke(cli, ["users", "all"])
         assert result.exit_code == 0
         assert "All users list" in result.output
+        self.travelperk.users.return_value.users.return_value.query.return_value.get.assert_called_once_with()
 
     # Webhooks
     def test_listing_webhook_operations(self):
@@ -397,6 +429,7 @@ class TestCli:
         result = self.runner.invoke(cli, ["webhooks", "all"])
         assert result.exit_code == 0
         assert "All webhooks list" in result.output
+        self.travelperk.webhooks.return_value.webhooks.return_value.all.assert_called_once_with()
 
     @patch("travelperk_cli.webhooks.commands.get_backend")
     def test_getting_details_of_a_webhook(self, get_backend_mock):
@@ -408,8 +441,9 @@ class TestCli:
         result = self.runner.invoke(cli, ["webhooks", "get", "--id", webhook_id])
         assert result.exit_code == 0
         assert "Details of a webhook" in result.output
-        # TODO
-        # asserting the function was called with the id
+        self.travelperk.webhooks.return_value.webhooks.return_value.get.assert_called_once_with(
+            webhook_id
+        )
 
     @patch("travelperk_cli.webhooks.commands.get_backend")
     def test_deleting_a_webhook(self, get_backend_mock):
@@ -423,6 +457,9 @@ class TestCli:
         assert "Delete webhook response" in result.output
         # TODO
         # asserting the function was called with the id
+        self.travelperk.webhooks.return_value.webhooks.return_value.delete.assert_called_once_with(
+            webhook_id
+        )
 
     @patch("travelperk_cli.webhooks.commands.get_backend")
     def test_creating_a_webhook(self, get_backend_mock):
@@ -430,23 +467,27 @@ class TestCli:
         self.travelperk.webhooks.return_value.webhooks.return_value.create.return_value = (
             "New webhook"
         )
+        name = "name"
+        url = "url"
+        secret = "secret"
         result = self.runner.invoke(
             cli,
             [
                 "webhooks",
                 "create",
                 "--name",
-                "Some name",
+                name,
                 "--url",
-                "Some url",
+                url,
                 "--secret",
-                "Some secret",
+                secret,
             ],
         )
         assert result.exit_code == 0
         assert "New webhook" in result.output
-        # TODO
-        # asserting the function was called with the id
+        self.travelperk.webhooks.return_value.webhooks.return_value.create.assert_called_once_with(
+            name, url, secret, ["invoice.issued"]  # TODO: This wont be hardcoded
+        )
 
     @patch("travelperk_cli.webhooks.commands.get_backend")
     def test_testing_a_webhook(self, get_backend_mock):
@@ -458,5 +499,6 @@ class TestCli:
         result = self.runner.invoke(cli, ["webhooks", "test", "--id", webhook_id])
         assert result.exit_code == 0
         assert "Testing webhook response" in result.output
-        # TODO
-        # asserting the function was called with the id
+        self.travelperk.webhooks.return_value.webhooks.return_value.test.assert_called_once_with(
+            webhook_id
+        )
